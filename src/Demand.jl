4f3890cfd140a2181c61ff6ed4807c23f85a98f7
SupplyChef <47842426+SupplyChef@users.noreply.github.com>
2023-12-20 09:34:28 -0500

    Add service level

--- src/Demand.jl
+++ src/Demand.jl
@@ -7,8 +7,9 @@ struct Demand
     demand::Array{Float64, 1}
     sales_price::Float64 # the revenue of a sale
     lost_sales_cost::Float64 # the total cost of a lost sales; including loss of overall business
+    service_level::Float64 # the desired service level
 
-    function Demand(customer::Customer, product::Product, demand::Array{Float64, 1}; sales_price=0.0, lost_sales_cost=0.0)
-        return new(customer, product, demand, sales_price, lost_sales_cost)
+    function Demand(customer::Customer, product::Product, demand::Array{Float64, 1}; sales_price=0.0, lost_sales_cost=0.0, service_level=1.0)
+        return new(customer, product, demand, sales_price, lost_sales_cost, service_level)
     end
 end
